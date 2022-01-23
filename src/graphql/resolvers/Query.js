ba5b1de9d4a14acb2347791ecf90f03bf987268b
Eren Çifci <eren.cifci04@gmail.com>
2022-01-23 18:47:28 +0300

    add language queries

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -21,4 +21,23 @@ export const Query = {
         const name = continents[code];
         return { code, name };
     },
+
+    // Language
+    languages: (_, __, { languages }) => {
+        return Object.entries(languages).map(([code, language]) => {
+            let rtl = false;
+            if (language.rtl) rtl = true;
+            return {
+                ...language,
+                code,
+                rtl,
+            };
+        });
+    },
+    language: (_, { code }, { languages }) => {
+        const language = languages[code];
+        let rtl = false;
+        if (language.rtl) rtl = true;
+        return { ...language, code, rtl };
+    },
 };
