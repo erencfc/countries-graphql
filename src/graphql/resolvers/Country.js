9cba76e2a331cfa0f0a03b749a1d0a9163697a00
Eren Çifci <eren.cifci04@gmail.com>
2022-01-23 18:58:51 +0300

    edit country resolver

--- src/graphql/resolvers/Country.js
+++ src/graphql/resolvers/Country.js
@@ -3,4 +3,15 @@ export const Country = {
         code: continent,
         name: continents[continent],
     }),
+    languages: ({ languages: langs }, __, { languages }) =>
+        langs.map((code) => {
+            const language = languages[code];
+            let rtl = false;
+            if (language.rtl) rtl = true;
+            return {
+                ...language,
+                code,
+                rtl,
+            };
+        }),
 };
